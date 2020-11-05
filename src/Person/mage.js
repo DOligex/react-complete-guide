1899251d8f0e83885ec865b578fcaf3d32c77184
Olivier Darbonville <odarbonville@explorance.com>
2020-11-05 16:27:33 -0500

    create miscellaneous components

---
+++ src/Person/mage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const mage = (props) => {
+    return <div>
+        <p>Je suis un mage et je m'appel {props.name}</p>
+    </div>
+}
+
+export default mage;
---
+++ src/Person/pala.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const pala = (props) => {
+    return <div>
+        <p>I ' m a Paladin and my name is {props.name}</p>
+    </div>
+}
+
+export default pala;
