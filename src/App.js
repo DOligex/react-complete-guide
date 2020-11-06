9e150a6802052e440900471ca36a2a33fd283908
Olivier Darbonville <odarbonville@explorance.com>
2020-11-06 16:30:34 -0500

    update App.js for toggle and dynamic content

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 // React components
 import Person from './Person/Person';
-
+import Personne from './Person/personne';
+import Pala from './Person/pala';
 class App extends Component {
   
   state = {
@@ -12,7 +13,9 @@ class App extends Component {
       { name: 'Barbie', age: 26 }
     ],
     otherState: 'some other value',
-    showPersons: false
+    showPersons: false,
+    showDk:false,
+    showPala: false
   }
   
   switchNameHandler = (newName) => {
@@ -40,6 +43,16 @@ class App extends Component {
     this.setState({showPersons: !doesShow});
   }
   
+  togglePersonalHandler = () => {
+    const doesShowDk = this.state.showDk;
+    this.setState({showDk: !doesShowDk});
+  }
+
+  togglePalaHandler = () => {
+    const doesShowPala = this.state.showPala;
+    this.setState({showPala: !doesShowPala})
+  }
+
   
   render() {
 
@@ -51,19 +64,22 @@ class App extends Component {
       cursor: 'pointer',
       boxShadow: `0px 2px 4px -1px rgba(0, 0, 0, 0.2),
       0px 4px 5px 0px rgba(0, 0, 0, 0.14),
-      0px 1px 10px 0px rgba(0, 0, 0, 0.12)`
+      0px 1px 10px 0px rgba(0, 0, 0, 0.12)`,
+      marginBottom: '50px'
     }
-    
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className="person">This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonHandler}>Toggle Persons</button>
-        {this.state.showPersons ? 
-          <div>
-            <Person
+
+    let persons = null;
+    let pala = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div className={style}>
+          {this.state.persons.map(person => {
+            return <Person 
+            name={person.name}
+            age={person.age}/>
+          })}
+            {/* <Person
               name={this.state.persons[0].name}
               age={this.state.persons[0].age}
               />
@@ -78,13 +94,62 @@ class App extends Component {
             <Person
               name={this.state.persons[2].name}
               age={this.state.persons[2].age}
-            />
-          </div> : null 
-        }
+              /> */}
+          </div>
+      )
+    }
 
+    if (this.state.showPala) {
+      pala = (
+        <div className={style}>
+          <Pala 
+            name="Patator"
+            age="Undefined ... ;0">
+          </Pala>
+
+        </div>
+      )
+    }
+    
+    return (
+      <div className="App">
+        <div className="first-part">
+
+        <h1>Hi, I'm a React App</h1>
+        <p className="person">This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonHandler}>Toggle Persons</button>
+        
+        {persons}
+          
+        </div>
+        <div>
+          <button
+            style={style}
+            onClick={this.togglePalaHandler}>
+            Paladin affiché / caché</button>
+        </div>
+
+        {pala}
+
+        <div>
+          <h3>Hi I'm the second part</h3>
+          <div>
+            <p>This part is a test for the toggle condition</p>
+            <button style={style}
+            onClick={this.togglePersonalHandler}>Toggle V2.0</button>
+            {this.state.showDk ?
+              <div>
+                <Personne
+                name="Krattos"
+                race="Dranei"
+                ilvl="400"> I'm a famous warrior</Personne>  
+              </div> : null  
+            }
+          </div>
+        </div>
       </div>
-      
-      
       );
     }
   }
