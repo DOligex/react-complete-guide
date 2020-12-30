057469ce1239d0055145769f8779a23cb3d1b0be
DOligex <olinours@yahoo.fr>
2020-12-30 16:34:39 -0500

    fix useless method and feat flexible component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,6 @@ const person = (props) => {
     <div className="Person">
         <p onClick={props.click}>I'm a person my name is {props.name} and I have {props.age} years old ! </p>
         <p>{props.children}</p>
-        {/* double binding  */}
         <input type="text" onChange={props.changed} value={props.name} />
     </div>
     )
